2f2ddad5b9c67dce2e4cae0d35887427b84dd941
White <whitek8327@gmail.com>
2023-09-30 11:59:44 +0700

    first commit

---
+++ ECommerceAPI/Bookstore.Core/BookServices.cs
@@ -0,0 +1,18 @@
+﻿namespace Bookstore.Core
+{
+    public class BookServices : IBookServices
+    {
+        public List<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book
+                {
+                    Name = "Test",
+                    Price = 10.2,
+                }
+            
+            };
+        }
+    }
+}
---
+++ ECommerceAPI/ECommerceAPI/Controllers/BooksController.cs
@@ -0,0 +1,25 @@
+using Bookstore.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookServices _bookServices;
+        public BooksController(IBookServices bookServices)
+        {
+            _bookServices = bookServices;
+        }
+
+        [HttpGet]
+        public IActionResult GetBooks() 
+        { 
+            
+            return Ok(_bookServices.GetBooks());
+        }
+
+
+    }
+}
